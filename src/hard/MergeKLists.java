90d75a31f1fe21a139e85442b5d8009d57e30511
Anindita Banik <anindita.banik@gmail.com>
2021-07-06 12:07:40 -0700

    Adding solutions

---
+++ src/hard/MergeKLists.java
@@ -0,0 +1,33 @@
+package hard;
+
+
+import java.util.PriorityQueue;
+
+//Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+public class MergeKLists {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<Integer> minHeap= new PriorityQueue<>();
+        for(ListNode head:lists) {
+            while(head!=null){
+                minHeap.add(head.val);
+                head = head.next;
+            }
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode head = dummy;
+        while(!minHeap.isEmpty()){
+            head.next = new ListNode(minHeap.remove());
+            head = head.next;
+        }
+        return dummy.next;
+    }
+}
